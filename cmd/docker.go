1d7758a6ab495217994bbe3a63ebfda5d54243f0
robin zhang <lit050528@gmail.com>
2023-12-12 08:11:42 +0800

    Update - add docker run cmd

--- cmd/docker.go
+++ cmd/docker.go
@@ -8,17 +8,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{Use: "docker"}
+var rootCmd = &cobra.Command{Use: "mydocker is a simple docker implemention"}
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Running docker init ...")
+	},
+}
 
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "",
+	Short: "Create a docker container",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Running docker run ...")
 	},
 }
 
 func init() {
+	runCmd.PersistentFlags().StringP("interactive", "i", "", "Keep STDIN open even if not attached")
+	runCmd.PersistentFlags().StringP("tty", "t", "", "Allocate a pseudo-TTY")
+
+	rootCmd.AddCommand(initCmd)
 	rootCmd.AddCommand(runCmd)
 }
 
